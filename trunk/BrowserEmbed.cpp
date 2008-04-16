2aec333876258c99ecbde5910c2571179f97375e
audiohacked <audiohacked@5abfc0d9-5d45-0410-8e86-dff7d036d72c>
2008-04-16 01:45:16 +0000

    [add] Added our own protocol to the browser.

--- trunk/BrowserEmbed.cpp
+++ trunk/BrowserEmbed.cpp
@@ -3,6 +3,17 @@
 sAppBrowser::sAppBrowser(wxWindow *parent, wxWindowID id, const wxPoint &pos, const wxSize &size) :
 	wxPanel(parent, id, pos, size)
 {
+
+	wxFileSystem::AddHandler(new wxInternetFSHandler());
+	wxFileSystem::AddHandler(new wxZipFSHandler());
+	wxFileSystem::AddHandler(new wxMemoryFSHandler());
+
+	const wxUId id = {(wxUint32)0x8a2bcf71,(wxUint16)0x3f5b,(wxUint16)0x4559,
+		{(wxUint8)0x89, (wxUint8)0x0f,(wxUint8)0xf3,(wxUint8)0xc2,(wxUint8)0xe9,(wxUint8)0x8e,
+		(wxUint8)0xa7,(wxUint8)0x01}};
+	
+	wxRegisterProtocolHandler(wxT("The DDPS protocol handler"), wxT("ddps"), id, &(ddpsProtocolHandler::Create));
+
 	browser = new wxMozillaBrowser(this, -1, wxDefaultPosition, wxDefaultSize, wxBORDER);
 
 	wxPanel *buttonPanel = new wxPanel(this, -1, wxDefaultPosition, wxDefaultSize);
@@ -25,4 +36,40 @@ sAppBrowser::sAppBrowser(wxWindow *parent, wxWindowID id, const wxPoint &pos, co
 	browser_buttons_sizer->Add(stop, 0, wxALL, 10);
 	browser_buttons_sizer->Add(home, 0, wxALL, 10);
 	SetSizer(sizer);
+}
+
+const wxString ddpsProtocolHandler::NewURI(const wxString spec, const wxString base)
+{
+
+	if(base.IsEmpty()||spec.StartsWith(wxT("ddps://")))
+		return spec;
+	
+	wxString mutable_base = base;
+	const int slash_position = base.Find(wxT('/'), TRUE);
+
+	if(slash_position>7)
+		if(!((base[slash_position-1]=='/')&&(base[slash_position-2]==':')))
+			mutable_base = base.Mid(0,slash_position);
+		else
+			mutable_base = base;
+	else
+		mutable_base = base;
+
+	if (!spec.StartsWith(wxT("/")))
+		mutable_base=mutable_base+wxT('/');
+
+	return mutable_base+spec;
+}
+
+wxProtocolChannel* ddpsProtocolHandler::OpenChannel(const wxString uri)
+{    
+	wxString after;
+	uri.StartsWith(wxT("ddps://"), &after); 
+
+	return wxFSProtocolHandler::OpenChannel(after);
+}
+
+static wxProtocolHandler* ddpsProtocolHandler::Create(const wxString scheme)
+{
+	return new ddpsProtocolHandler(scheme);
 }
