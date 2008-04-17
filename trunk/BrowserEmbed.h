a31f69014e0e2e72797f37db68b2d1b4e7b3cce5
audiohacked <audiohacked@5abfc0d9-5d45-0410-8e86-dff7d036d72c>
2008-04-17 17:30:35 +0000

    [fix/add] fixed broken browser and added libs and includes for wxMozilla; SSL/HTTPS problem still persists

--- trunk/BrowserEmbed.h
+++ trunk/BrowserEmbed.h
@@ -5,32 +5,36 @@
 		#include <wx/wx.h>
 	#endif
 	
+	
+	#include <wxmozilla/wxMozilla.h>
+/*
 	#include <wx/filesys.h>
 	#include <wx/fs_inet.h>
 	#include <wx/fs_zip.h>
 	#include <wx/fs_mem.h>
-	
-	#include <wxmozilla/wxMozilla.h>
 	#include <wxmozilla/wxMozillaProtocolHandler.h>
-	
+*/	
 	class sAppBrowser : public wxPanel
 	{
 		public:
 			sAppBrowser(wxWindow *parent, wxWindowID id, const wxPoint &pos, const wxSize &size);
 			wxMozillaBrowser *browser;
 	};
-	
-	class ddpsProtocolHandler : public wxFSProtocolHandler
+
+	/*
+	class ddpsProtocolHandler : public wxProtocolHandler
 	{
 		public:
-			ddpsProtocolHandler(const wxString scheme) : wxFSProtocolHandler(scheme){}
+			ddpsProtocolHandler(const wxString scheme) : wxProtocolHandler(scheme){}
 			~ddpsProtocolHandler(){}
-			const wxString NewURI(const wxString spec, const wxString base)
-			wxProtocolChannel* OpenChannel(const wxString uri)
-			static wxProtocolHandler* Create(const wxString scheme)
+			const wxString NewURI(const wxString spec, const wxString base);
+			wxProtocolChannel* OpenChannel(const wxString uri);
+			static wxProtocolHandler* Create(const wxString scheme){return new ddpsProtocolHandler(scheme);}
+
 		
 		protected:
 			wxFileSystem mFileSystem;
 
 	};
+	*/
 #endif
