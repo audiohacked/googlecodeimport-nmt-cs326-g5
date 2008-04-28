4592b1795b79850f320c8e8a54596c173d069cae
audiohacked <audiohacked@5abfc0d9-5d45-0410-8e86-dff7d036d72c>
2008-04-28 21:57:50 +0000

    [fix] insure that we goto the news page in-case that we navigated away from the news page

--- BrowserEmbed.cpp
+++ BrowserEmbed.cpp
@@ -12,16 +12,6 @@ END_EVENT_TABLE()
 sAppBrowser::sAppBrowser(wxWindow *parent, wxWindowID id, const wxPoint &pos, const wxSize &size) :
 	wxPanel(parent, id, pos, size)
 {
-/*
-	wxFileSystem::AddHandler(new wxInternetFSHandler());
-	wxFileSystem::AddHandler(new wxZipFSHandler());
-	wxFileSystem::AddHandler(new wxMemoryFSHandler());
-
-	const wxUId proto_id = {(wxUint32)0x8a2bcf71,(wxUint16)0x3f5b,(wxUint16)0x4559,
-		{(wxUint8)0x89, (wxUint8)0x0f,(wxUint8)0xf3,(wxUint8)0xc2,(wxUint8)0xe9,(wxUint8)0x8e,
-		(wxUint8)0xa7,(wxUint8)0x01}};
-	wxRegisterProtocolHandler(wxT("The DDPS protocol handler"), wxT("ddps"), proto_id, &(ddpsProtocolHandler::Create));
-*/
 
 	browser = new wxMozillaBrowser(this, -1, wxDefaultPosition, wxDefaultSize, wxBORDER);
 	
@@ -91,35 +81,4 @@ void sAppBrowser::GotoHomepage(void)
 	printf("GotoHomepage..\n");
 	browser->LoadURL(home);	
 }
-/*
-const wxString ddpsProtocolHandler::NewURI(const wxString spec, const wxString base)
-{
 
-	if(base.IsEmpty()||spec.StartsWith(wxT("ddps://")))
-		return spec;
-	
-	wxString mutable_base = base;
-	const int slash_position = base.Find(wxT('/'), TRUE);
-
-	if(slash_position>7)
-		if(!((base[slash_position-1]=='/')&&(base[slash_position-2]==':')))
-			mutable_base = base.Mid(0,slash_position);
-		else
-			mutable_base = base;
-	else
-		mutable_base = base;
-
-	if (!spec.StartsWith(wxT("/")))
-		mutable_base=mutable_base+wxT('/');
-
-	return mutable_base+spec;
-}
-
-wxProtocolChannel* ddpsProtocolHandler::OpenChannel(const wxString uri)
-{    
-	wxString after;
-	uri.StartsWith(wxT("ddps://"), &after); 
-
-	return wxProtocolHandler::OpenChannel(after);
-}
-*/
--- BrowserEmbed.h
+++ BrowserEmbed.h
@@ -5,14 +5,14 @@
 		#include <wx/wx.h>
 	#endif
 	
-	#include <contrib/includes/wxmozilla/wxMozilla.h>
-/*
 	#include <wx/filesys.h>
 	#include <wx/fs_inet.h>
 	#include <wx/fs_zip.h>
 	#include <wx/fs_mem.h>
+
+	#include <wxmozilla/wxMozillaBrowser.h>
 	#include <wxmozilla/wxMozillaProtocolHandler.h>
-*/	
+
 	class sAppBrowser : public wxPanel
 	{
 		public:
@@ -32,20 +32,57 @@
 			
 	};
 
-	/*
-	class ddpsProtocolHandler : public wxProtocolHandler
+
+	class ddpsProtocolHandler : public wxFSProtocolHandler
 	{
 		public:
-			ddpsProtocolHandler(const wxString scheme) : wxProtocolHandler(scheme){}
-			~ddpsProtocolHandler(){}
-			const wxString NewURI(const wxString spec, const wxString base);
-			wxProtocolChannel* OpenChannel(const wxString uri);
-			static wxProtocolHandler* Create(const wxString scheme){return new ddpsProtocolHandler(scheme);}
+			ddpsProtocolHandler(const wxString scheme) : wxFSProtocolHandler(scheme)
+			{}
+			
+			~ddpsProtocolHandler()
+			{}
+			
+			static wxProtocolHandler* Create(const wxString scheme)
+			{
+				return new ddpsProtocolHandler(scheme);
+			}
+			
+			const wxString NewURI(const wxString spec, const wxString base)
+			{
 
-		
+				if(base.IsEmpty()||spec.StartsWith(wxT("ddps://")))
+					return spec;
+
+				wxString mutable_base = base;
+				const int slash_position = base.Find(wxT('/'), TRUE);
+
+				if(slash_position>7)
+					if(!((base[slash_position-1]=='/')&&(base[slash_position-2]==':')))
+						mutable_base = base.Mid(0,slash_position);
+					else
+						mutable_base = base;
+				else
+					mutable_base = base;
+
+				if (!spec.StartsWith(wxT("/")))
+					mutable_base=mutable_base+wxT('/');
+
+				return mutable_base+spec;
+			}
+
+			wxProtocolChannel* OpenChannel(const wxString uri)
+			{    
+				wxString after;
+				uri.StartsWith(wxT("ddps://"), &after); 
+
+				return wxFSProtocolHandler::OpenChannel(after);
+			}
+
 		protected:
 			wxFileSystem mFileSystem;
 
 	};
-	*/
+
 #endif
+
+
