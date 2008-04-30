023f9f8eb1b6999ec439783fa2a76f8c742c27f6
audiohacked <audiohacked@5abfc0d9-5d45-0410-8e86-dff7d036d72c>
2008-04-30 16:45:57 +0000

    [fix] ddps protocol compile errors on linux due to: undefined reference to 'typeinfo for wxFSProtocolHandler'

--- AppMain.cpp
+++ AppMain.cpp
@@ -30,18 +30,19 @@ IMPLEMENT_APP(sApp)
 
 bool sApp::OnInit()
 {
+	#ifdef __WX_MOZILLA_PROTOCOLS_H__
 	wxFileSystem::AddHandler(new wxInternetFSHandler());
 	wxFileSystem::AddHandler(new wxZipFSHandler());
 	wxFileSystem::AddHandler(new wxMemoryFSHandler());
-
-	sAppFrame *frame = new sAppFrame(_T("sAppFrame"), wxPoint(50, 50), wxSize(800,600));
 
 	const wxUId id = {(wxUint32)0x8a2bcf71,(wxUint16)0x3f5b,(wxUint16)0x4559,
 		{(wxUint8)0x89, (wxUint8)0x0f,(wxUint8)0xf3,(wxUint8)0xc2,(wxUint8)0xe9,(wxUint8)0x8e,
 		(wxUint8)0xa7,(wxUint8)0x01}};
 	
 	wxRegisterProtocolHandler(wxT("The DDPS protocol handler"), wxT("ddps"), id, &(ddpsProtocolHandler::Create));
-		
+	#endif
+	
+	sAppFrame *frame = new sAppFrame(_T("sAppFrame"), wxPoint(50, 50), wxSize(800,600));
 	frame->Show(TRUE);
 	SetTopWindow(frame);
 	return TRUE;
