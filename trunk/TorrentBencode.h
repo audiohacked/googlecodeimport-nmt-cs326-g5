51bb2692e0f6c8908f0209f745b1172d503ac8af
audiohacked <audiohacked@5abfc0d9-5d45-0410-8e86-dff7d036d72c>
2008-06-26 23:59:11 +0000

    [fix] oops, for got the bencode algorithm files

---
+++ trunk/TorrentBencode.h
@@ -0,0 +1,25 @@
+#ifndef __TORRENT_BENCODE_H
+#define __TORRENT_BENCODE_H
+
+	#include <wx/wxprec.h>
+
+	#ifndef WX_PRECOMP
+		#include <wx/wx.h>
+	#endif
+
+	#include <wx/datstrm.h>
+	#include <wx/wfstream.h>
+	#include <wx/ffile.h>
+	#include <wx/txtstrm.h>
+
+	#include <boost/lexical_cast.hpp>
+
+	#include <libtorrent/config.hpp>
+	#include <libtorrent/entry.hpp>
+
+	using namespace libtorrent;
+
+	int wx_bdecode(wxFileInputStream &file, wxDataInputStream &data, entry &ret, int depth);
+	void wx_bencode(wxDataOutputStream &out_data, wxTextOutputStream &out_text, entry e);
+
+#endif
