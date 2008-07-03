670a5fa911674ba3878bd85c56ae9bfede70fb17
audiohacked <audiohacked@5abfc0d9-5d45-0410-8e86-dff7d036d72c>
2008-07-03 22:26:27 +0000

    [cleanup] comment out some (unused?) boost headers

--- trunk/TorrentManager.cpp
+++ trunk/TorrentManager.cpp
@@ -8,13 +8,13 @@
 #include <wx/wx.h>
 #endif
 
-#include <boost/filesystem/operations.hpp>
-#include <boost/filesystem/convenience.hpp>
-#include <boost/filesystem/fstream.hpp>
-#include <boost/filesystem/exception.hpp>
-#include <boost/bind.hpp>
-#include <boost/program_options.hpp>
-#include <boost/regex.hpp>
+//#include <boost/filesystem/operations.hpp>
+//#include <boost/filesystem/convenience.hpp>
+//#include <boost/filesystem/fstream.hpp>
+//#include <boost/filesystem/exception.hpp>
+//#include <boost/bind.hpp>
+//#include <boost/program_options.hpp>
+//#include <boost/regex.hpp>
 
 #include <libtorrent/config.hpp>
 #include <libtorrent/version.hpp>
