6dc5f94d1e6c9c791f0b3bb80f64454c4134fd0b
audiohacked <audiohacked@5abfc0d9-5d45-0410-8e86-dff7d036d72c>
2009-03-02 20:43:02 +0000

    VS2008 custom commands; work in progress

--- contrib/ddps_custom_commands.cmake
+++ contrib/ddps_custom_commands.cmake
@@ -1,20 +1,5 @@
-INCLUDE( ${CMAKE_SOURCE_DIR}/contrib/CopyIfDifferent.cmake )
-SET(BUTTON_BMPS
-	icon_button_friends.bmp
-	icon_button_friends_mousedown.bmp
-	icon_button_friends_mouseover.bmp
-	icon_button_news.bmp
-	icon_button_news_mousedown.bmp
-	icon_button_news_mouseover.bmp
-	icon_button_settings.bmp
-	icon_button_settings_mousedown.bmp
-	icon_button_settings_mouseover.bmp
-	icon_button_support.bmp
-	icon_button_support_mousedown.bmp
-	icon_button_support_mouseover.bmp
+ADD_CUSTOM_TARGET(CopyAppBmp ALL 
+	COMMAND ${CMAKE_COMMAND} -E copy_directory 
+	${CMAKE_SOURCE_DIR}/images 
+	${CMAKE_BINARY_DIR}/${CMAKE_CFG_INTDIR}/images
 )
-COPY_IF_DIFFERENT(
-	${CMAKE_SOURCE_DIR}/images/
-	${CMAKE_BUILD_DIR}/${CMAKE_BUILD_TYPE}/images/
-	${BUTTON_BMPS} ButtonBmpCopy "ButtonBmps")
-#ADD_TARGET(CopyButtonBmp ALL DEPENDS ${ButtonBmpCopy})
