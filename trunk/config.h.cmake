e50ec40fd1c44a4f5e03b2f3489df43bb97dc5c1
audiohacked <audiohacked@5abfc0d9-5d45-0410-8e86-dff7d036d72c>
2009-04-29 14:49:31 +0000

    reorganize program c defines

--- trunk/config.h.cmake
+++ trunk/config.h.cmake
@@ -1,42 +1,22 @@
 #ifndef __CONFIG_H
 #define __CONFIG_H
-
 	#cmakedefine BUILTIN_BROWSER 1
-
 	#cmakedefine CHAT_ENABLED 1
-
 	#cmakedefine CHAT_SERVER "${CHAT_SERVER}"
-
 	#cmakedefine COMMUNITY_PORTAL 1
-
 	#cmakedefine COMMUNITYPAGE "${COMMUNITYPAGE}"
-
 	#cmakedefine DEBUG_MENU 1
-
 	#cmakedefine DEVEL_TESTING 1
-
 	#cmakedefine DOWNLOADER 1
-	
-	#cmakedefine IMG_BUTTONS 1
-
 	#cmakedefine HOMEPAGE "${HOMEPAGE}"
-
 	#cmakedefine HTTP_DOWNLOADER 1
-
-	#cmakedefine TORRENT 1
-
-	#cmakedefine TORRENT_DOWNLOADER 1
-	
-	#cmakedefine UPDATER 1
-	
+	#cmakedefine IMG_BUTTONS 1
 	#cmakedefine NEWSPAGE "${NEWSPAGE}"
-
 	#cmakedefine SUPPORTPAGE "${SUPPORTPAGE}"
-
+	#cmakedefine TORRENT 1
+	#cmakedefine TORRENT_DOWNLOADER 1
+	#cmakedefine UPDATER 1
 	#cmakedefine UPDATEURL "${UPDATEURL}"
-
 	#cmakedefine USER_UPLOAD 1
-
 	#cmakedefine USE_WEBKIT 1
-	
 #endif
